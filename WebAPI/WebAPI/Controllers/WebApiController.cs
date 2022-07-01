8dea3efa3c0a2d5ab19740c4785357c050a030cd
Azib <Azib@IMRANSAEED>
2022-07-01 17:35:59 +0500

    webapi controller work

---
+++ WebAPI/WebAPI/Controllers/WebApiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+using System.Data.Entity;
+
+namespace WebAPI.Controllers
+{
+    public class WebApiController : ApiController
+    {
+        LostandFoundEntities db = new LostandFoundEntities();
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetData()
+        {
+            List<App_Admin> list = db.App_Admin.ToList();
+            return Ok(list);
+        }
+    }
+}
